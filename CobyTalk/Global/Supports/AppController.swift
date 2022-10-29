ae58c19982ff5bd5253770ec9e53ec0054a4892b
coby5502 <coby5502@gmail.com>
2022-10-30 02:22:19 +0900

    [Feat] 파베 로그인

--- CobyTalk/Global/Supports/AppController.swift
+++ CobyTalk/Global/Supports/AppController.swift
@@ -5,4 +5,53 @@
 //  Created by Coby Kim on 2022/10/30.
 //
 
-import Foundation
+import UIKit
+import Firebase
+
+final class AppController {
+    static let shared = AppController()
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    private var window: UIWindow!
+    private var rootViewController: UIViewController? {
+        didSet {
+            window.rootViewController = rootViewController
+        }
+    }
+    
+    init() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handleAppState),
+            name: .AuthStateDidChange,
+            object: nil)
+    }
+    
+    // MARK: - Helpers
+    func configureFirebase() {
+        FirebaseApp.configure()
+    }
+    
+    func show(in window: UIWindow?) {
+        guard let window = window else {
+            fatalError("Cannot layout app with a nil window.")
+        }
+        
+        self.window = window
+        window.tintColor = .black
+        window.backgroundColor = .white
+        
+        handleAppState()
+        
+        window.makeKeyAndVisible()
+    }
+    
+    // MARK: - Notifications
+    @objc private func handleAppState() {
+        if let user = Auth.auth().currentUser {
+//            let channelsViewController = ChannelsViewController(currentUser: user)
+//            rootViewController = NavigationController(channelsViewController)
+        } else {
+            rootViewController = LoginViewController()
+        }
+    }
+}
--- CobyTalk/Screens/Login/LoginViewController.swift
+++ CobyTalk/Screens/Login/LoginViewController.swift
@@ -7,18 +7,106 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+import FirebaseAuth
+import SnapKit
+import Then
 
+final class LoginViewController: UIViewController {
+    
+    private lazy var displayNameField = UITextField().then {
+        let attributes = [
+            NSAttributedString.Key.foregroundColor : UIColor.black,
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17, weight: .regular)
+        ]
+        
+        $0.backgroundColor = .white
+        $0.attributedPlaceholder = NSAttributedString(string: "Display Name", attributes: attributes)
+        $0.autocapitalizationType = .none
+        $0.layer.cornerRadius = 12
+        $0.layer.masksToBounds = true
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        $0.leftViewMode = .always
+        $0.clipsToBounds = false
+        $0.addTarget(
+            self,
+            action: #selector(textFieldDidReturn),
+            for: .primaryActionTriggered)
+    }
+    
+    private lazy var logInbutton = UIButton().then {
+        $0.backgroundColor = .black
+        $0.setTitleColor(.white, for: .normal)
+        $0.setTitle("Log In", for: .normal)
+        $0.addTarget(
+            self,
+            action: #selector(actionButtonPressed),
+            for: .primaryActionTriggered)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let test = UILabel()
-        view.backgroundColor = .white // 배경색
-        view.addSubview(test)
-        test.text = "text" // test를 위해서 출력할 라벨
-        test.translatesAutoresizingMaskIntoConstraints = false
-        test.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        test.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        view.addSubview(displayNameField)
+        view.addSubview(logInbutton)
+        
+        displayNameField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(30)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        logInbutton.snp.makeConstraints {
+            $0.top.equalTo(displayNameField.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(60)
+        }
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        displayNameField.becomeFirstResponder()
+    }
+    
+    // MARK: - Actions
+    @IBAction private func actionButtonPressed() {
+        signIn()
+    }
+    
+    @objc private func textFieldDidReturn() {
+        signIn()
+    }
+    
+    private func signIn() {
+        guard
+            let name = displayNameField.text,
+            !name.isEmpty
+        else {
+            showMissingNameAlert()
+            return
+        }
+        
+        displayNameField.resignFirstResponder()
+        
+        AppSettings.displayName = name
+        Auth.auth().signInAnonymously()
+    }
+    
+    private func showMissingNameAlert() {
+        let alertController = UIAlertController(
+            title: "Display Name Required",
+            message: "Please enter a display name.",
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: "Okay",
+            style: .default) { _ in
+                self.displayNameField.becomeFirstResponder()
+            }
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
 }
